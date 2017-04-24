5e85344734116e63cf6802f26935df128dab2edb
Minji Lee <minjilee8816@gmail.com>
2017-04-24 13:26:02 -0700

    minimal setup for Express server serving static files

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require ('http'); 
+const express =require('express'); 
+const app = express(); 
+
+
+app.use(express.static('public'))
+app.set('port', process.env.PORT || 8080); 
+
+
+const server = http.createServer(app);
+
+server.listen(app.get('port'), () =>{
+	console.log(`Server listening on port ${app.get('port')}`); 
+});
+
